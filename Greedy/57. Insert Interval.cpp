3909b83349785d95e3b2eb09e22cf6510b3e214b
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-07-16 20:51:51 +0530

    Update 57. Insert Interval.cpp

--- Greedy/57. Insert Interval.cpp
+++ Greedy/57. Insert Interval.cpp
@@ -36,3 +36,35 @@ public:
         return res;
     }
 };
+
+
+// GREEDY
+// APPROACH 2
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>>res;
+        int i=0;
+        int n = intervals.size();
+        while(i < n && newInterval[0] > intervals[i][1]) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+        
+        // merging the overlapping intervals;
+        while(i < n && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+        
+        // merging ends here
+        while(i < n) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+        
+        return res;
+    }
+};
